c1449e52d9da68a0755bda9c7360ae83c4246189
Arpit <arpitgupta455@gmail.com>
2024-04-01 18:28:42 +0530

    Create Length of Last Word.cpp

---
+++ 2024/APR/Length of Last Word.cpp
@@ -0,0 +1,13 @@
+int lengthOfLastWord(string s) {
+
+        int n=s.size()-1,count=0;
+        while(s[n]==' ')
+        n--;
+        while(n>=0 && s[n]!=' ')
+        {
+            count++;
+            n--;
+        }
+        return count;
+        
+    }
