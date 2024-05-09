00751b115444af9e48f37b8634542cc26dcc0e6c
Arpit <arpitgupta455@gmail.com>
2024-05-09 09:20:27 +0530

    Create Maximize Happiness of Selected Children.cpp

---
+++ 2024/MAY/Maximize Happiness of Selected Children.cpp
@@ -0,0 +1,12 @@
+long long maximumHappinessSum(vector<int>& happiness, int k) {
+
+        sort(happiness.begin(),happiness.end(),greater<int>());
+        long long ans=0;
+        for(int i=0;i<k;i++)
+        {
+          if(happiness[i]-i>0)
+          ans+=happiness[i]-i;
+        }
+        return ans;
+        
+    }
