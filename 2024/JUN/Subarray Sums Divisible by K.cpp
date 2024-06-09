041a748aeb64e79e2fa6058d972fc0b45ae1d57f
Arpit <arpitgupta455@gmail.com>
2024-06-09 14:29:48 +0530

    Create Subarray Sums Divisible by K.cpp

---
+++ 2024/JUN/Subarray Sums Divisible by K.cpp
@@ -0,0 +1,19 @@
+ int subarraysDivByK(vector<int>& nums, int k) {
+
+        unordered_map<int,int>m;
+        int j=0;
+        int sum=0,ans=0;
+        while(j<nums.size())
+        {
+            sum+=nums[j];
+            if(sum%k==0)
+             ans++;
+            if(m.find((sum%k+k)%k)!=m.end())
+            ans+=m[(sum%k+k)%k];
+             m[(sum%k+k)%k]++;
+            j++;
+            
+        }
+        return ans;
+        
+    }
