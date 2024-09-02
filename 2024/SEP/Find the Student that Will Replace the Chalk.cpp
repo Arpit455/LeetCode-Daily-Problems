a42e08341b9676cc5dc39c35adf8b8a30da99d56
Arpit <arpitgupta455@gmail.com>
2024-09-02 11:35:45 +0530

    Create Find the Student that Will Replace the Chalk.cpp

---
+++ 2024/SEP/Find the Student that Will Replace the Chalk.cpp
@@ -0,0 +1,17 @@
+int chalkReplacer(vector<int>& chalk, int k) {
+
+        long long int sum=0;
+        for(auto i:chalk){
+        sum+=i;
+        }
+
+        k=k%sum;
+        for(int i=0;i<chalk.size();i++)
+        {
+            if(chalk[i]>k)
+            return i;
+            k-=chalk[i];
+        }
+        return 0;
+        
+    }
