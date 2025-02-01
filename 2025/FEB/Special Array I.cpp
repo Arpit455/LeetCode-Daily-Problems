24b5b9bdf0c8b6261437d72745c49c1b697fac97
Arpit <arpitgupta455@gmail.com>
2025-02-01 16:19:26 +0530

    Create Special Array I.cpp

---
+++ 2025/FEB/Special Array I.cpp
@@ -0,0 +1,10 @@
+bool isArraySpecial(vector<int>& nums) {
+
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(!((nums[i]+nums[i+1])&1))
+            return false;
+        }
+        return true;
+        
+    }
