db65c3af75abb94ba59e477ff52c847db3e53ccb
Arpit <arpitgupta455@gmail.com>
2025-02-14 10:36:26 +0530

    Update Product of the Last K Numbers.cpp

--- 2025/FEB/Product of the Last K Numbers.cpp
+++ 2025/FEB/Product of the Last K Numbers.cpp
@@ -25,10 +25,3 @@ vector<int>product;
 
     }
 };
-
-/**
- * Your ProductOfNumbers object will be instantiated and called as such:
- * ProductOfNumbers* obj = new ProductOfNumbers();
- * obj->add(num);
- * int param_2 = obj->getProduct(k);
- */
