bdc6abe59b0c32b1c14936e2e251cd6239c7ffef
Arpit <arpitgupta455@gmail.com>
2025-07-17 23:04:27 +0530

    Update Find the Maximum Length of Valid Subsequence II.cpp

--- 2025/JUL/Find the Maximum Length of Valid Subsequence II.cpp
+++ 2025/JUL/Find the Maximum Length of Valid Subsequence II.cpp
@@ -1,10 +1,4 @@
-You have a nested dictionary structure representing a company’s organizational hierarchy. Each key is a department name, and the value can either be an integer representing the number of employees or another dictionary representing sub-departments.
-
-Example:
-
-org_structure = { ‘Sales’: 150, ‘Engineering’: { ‘Software’: { ‘Backend’: 30, ‘Frontend’: 20 }, ‘Hardware’: 50 }, ‘HR’: 40 } Task:
-
-Write a recursive function that traverses the dictionary and calculates the total number of employees in the organization. Modify the function to also return a dictionary that contains the number of employees in each department, including aggregated counts for departments with sub-departments.class Solution {
+class Solution {
 public:
    
     int maximumLength(vector<int>& nums, int k) {
